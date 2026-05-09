58d72025494b306af786a0dab538bf0b80f1567f
adylovMaksat <maksatbekadylov@gmail.com>
2026-05-09 19:54:00 +0200

    Implemented bottom tabs

--- App.tsx
+++ App.tsx
@@ -1,9 +1,36 @@
-import { Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import HomeScreen from './src/screens/HomeScreen';
+import AddEntryScreen from './src/screens/AddEntryScreen';
+import DetailsScreen from './src/screens/DetailsScreen';
+
+const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+
+function TabsNavigator() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Add Entry" component={AddEntryScreen} />
+    </Tab.Navigator>
+  );
+}
 
 export default function App() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Travel Journal App</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="MainTabs"
+          component={TabsNavigator}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
--- src/screens/DetailsScreen.tsx
+++ src/screens/DetailsScreen.tsx
@@ -1,9 +1,9 @@
 import { Text, View } from 'react-native';
 
-export default function AddEntryScreen() {
+export default function DetailsScreen() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Add Entry Screen</Text>
+      <Text>Details Screen</Text>
     </View>
   );
 }
